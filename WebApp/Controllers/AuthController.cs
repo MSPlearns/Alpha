2093c4898a36b3c70af4bccdf073e5723b07c419
MSPlearns <mspinolapulido@gmail.com>
2025-03-15 23:57:48 +0100

    Add project files.

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return LocalRedirect("/projects");
+            //return View();
+        }
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
