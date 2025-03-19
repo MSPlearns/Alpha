d05ddc241b727162450c6720dd92b1255b274bc2
MSPlearns <mspinolapulido@gmail.com>
2025-03-19 20:03:19 +0100

    Done clinelist partial, started registrering form.

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -6,9 +7,27 @@ namespace WebApp.Controllers
     {
         public IActionResult Register()
         {
-            return LocalRedirect("/projects");
-            //return View();
+            //return LocalRedirect("/projects");
+            var viewModel = new RegisterViewModel();
+            return View(viewModel);
         }
+        [HttpPost]
+        public IActionResult Register(RegisterFormModel formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                RegisterViewModel viewModel = new()
+                {
+                    FormData = formData
+                };
+                return View(viewModel);
+            }
+
+            return View();
+
+            //return LocalRedirect("/projects");
+        }
+
         [Route("login")]
         public IActionResult Login()
         {
---
+++ WebApp/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.Models;
+
+public class RegisterViewModel
+{
+    public RegisterFormModel FormData { get; set; } = new();
+    public List<SelectListItem> ClientOptions{ get; set; } = [];
+
+    public RegisterViewModel()
+    {
+
+    }
+
+        public async Task PopulateClientsAsync()
+    {
+        // This is where you would query the database for the clients
+        // and populate the Clients property with the results
+
+    }
+}
