b4f17a8024687702783168aa8fe4802264bfef28
MSPlearns <mspinolapulido@gmail.com>
2025-04-23 19:57:45 +0200

    BaseRepository complete with sort, filter include

--- Data/Repositories/BaseRepository.cs
+++ Data/Repositories/BaseRepository.cs
@@ -7,10 +7,23 @@ using Shared.Results;
 
 namespace Data.Repositories;
 
-// TODO: Add a generic interface for the repository
-// TODO: Add sorting, filtering and including to the GetAllAsync method
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task AddAsync(TEntity entity);
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    void Delete(TEntity entity);
+    Task<bool> EntityExistsAsync(Expression<Func<TEntity, bool>> expression);
+    Task<Result> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? where = null, params Expression<Func<TEntity, object>>[] includes);
+    Task<Result> GetAllAsync<TSelect>(Expression<Func<TEntity, TSelect>> selector, bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? where = null, params Expression<Func<TEntity, object>>[] includes);
+    Task<Result> GetAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includes);
+    Task RollbackTransactionAsync();
+    Task<int> SaveAsync();
+    Task<Result> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity updatedEntity);
+}
+
 // The methods are virtual so that i can implement eager loading
-public class BaseRepository<TEntity, TModel>(AppDbContext context) where TEntity : class where TModel : class
+public class BaseRepository<TEntity, TModel>(AppDbContext context) : IBaseRepository<TEntity> where TEntity : class where TModel : class
 {
     protected readonly AppDbContext _context = context;
 
@@ -54,51 +67,122 @@ public class BaseRepository<TEntity, TModel>(AppDbContext context) where TEntity
     }
 
     // Read
-    public virtual async Task<Result> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>>? includeExpression = null) 
+
+    public virtual async Task<Result> GetAllAsync
+        (bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? where = null, params Expression<Func<TEntity, object>>[] includes)
     {
+
         IQueryable<TEntity> query = _dbSet;
-        if (includeExpression != null)
+        if (where != null)
         {
-            query = includeExpression(query);
+            query = query.Where(where);
         }
-            var result = await query.ToListAsync();
 
-        if (result.Count == 0)
+        if (includes != null && includes.Length > 0)
+        {
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+
+        if (sortBy != null)
+        {
+            query = orderByDescending
+                ? query.OrderByDescending(sortBy)
+                : query.OrderBy(sortBy);
+        }
+
+        var entities = await query.ToListAsync();
+        if (entities.Count == 0)
+        {
             return Result.NotFound("No entities found");
-
-        var mappedResult = result.Select(x => x.MapTo<TModel>()).ToList();
-        return RepositoryResult<IEnumerable<TModel>>.Ok(mappedResult);
+        }
+        var result = entities.Select(entitiy => entitiy.MapTo<TModel>()).ToList();
+        return RepositoryResult<IEnumerable<TModel>>.Ok(result);
     }
 
-    public virtual async Task<Result> GetAsync(Expression<Func<TEntity, bool>> expression, Func<IQueryable<TEntity>, IQueryable<TEntity>>? includeExpression = null)
+
+    public virtual async Task<Result> GetAllAsync<TSelect>
+    (
+        Expression<Func<TEntity, TSelect>> selector,
+    bool orderByDescending = false,
+    Expression<Func<TEntity, object>>? sortBy = null,
+    Expression<Func<TEntity, bool>>? where = null,
+    params Expression<Func<TEntity, object>>[] includes
+    )
+    {
+
+        IQueryable<TEntity> query = _dbSet;
+        if (where != null)
+        {
+            query = query.Where(where);
+        }
+
+        if (includes != null && includes.Length > 0)
+        {
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+
+        if (sortBy != null)
+        {
+            query = orderByDescending
+                ? query.OrderByDescending(sortBy)
+                : query.OrderBy(sortBy);
+        }
+
+        var entities = await query.Select(selector).ToListAsync();
+        if (entities.Count == 0)
+        {
+            return Result.NotFound("No entities found.");
+        }
+        //var result = entities.Select(entitiy => entitiy!.MapTo<TSelect>()).ToList();
+        return RepositoryResult<IEnumerable<TSelect>>.Ok(entities);
+    }
+
+
+
+
+
+
+    public virtual async Task<Result> GetAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includes)
     {
         IQueryable<TEntity> query = _dbSet;
-        if (includeExpression != null)
-        {
-            query = includeExpression(query);
-        }
-        var result = await query.FirstOrDefaultAsync(expression);
-        if (result == null)
-            return Result.NotFound("Entity not found");
 
-        var mappedResult = result.MapTo<TModel>();
-        return RepositoryResult<TModel>.Ok(mappedResult);
+        if (includes != null && includes.Length > 0)
+        {
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+        var entity = await query.FirstOrDefaultAsync(where);
+
+        if (entity == null)
+            return Result.NotFound("Entity not found.");
+
+        var result = entity.MapTo<TModel>();
+        return RepositoryResult<TModel>.Ok(result);
     }
 
 
     // Update
-    public virtual async Task<Result> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity updatedEntity) 
+    public virtual async Task<Result> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity updatedEntity)
     {
         var existingEntity = await _dbSet.FirstOrDefaultAsync(expression);
         if (existingEntity == null)
-        { 
-           return Result.NotFound("Entity not found");
-        }  
+        {
+            return Result.NotFound("Entity not found.");
+        }
         _context.Entry(existingEntity).CurrentValues.SetValues(updatedEntity);
         return Result.Ok();
     }
 
     // Delete
+    // TODO: Delete by id
     public virtual void Delete(TEntity entity)
     {
         _dbSet.Remove(entity);
---
+++ Data/Repositories/ClientRepository.cs
@@ -0,0 +1,24 @@
+﻿using Data.Context;
+using Data.Entities;
+using Domain.Models;
+
+namespace Data.Repositories;
+public interface IClientRepository : IBaseRepository<ClientEntity, Client>
+{
+}
+public class ClientRepository(AppDbContext context) : BaseRepository<ClientEntity, Client>(context), IClientRepository
+{
+}
+
+public class AppUserRepository(AppDbContext context) : BaseRepository<AppUserEntity, AppUser>(context)
+{
+}
+
+public class StatusRepository(AppDbContext context) : BaseRepository<StatusEntity, Status>(context)
+{
+}
+
+public class ProjectRepository(AppDbContext context) : BaseRepository<ProjectEntity, Project>(context)
+{
+    //UserRepository eller UserManager
+}
